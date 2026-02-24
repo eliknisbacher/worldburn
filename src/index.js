90a341710bdf0ab9e90036c2542ef9b6e65f8440
emmaknisbacher <emmaknisbacher@gmail.com>
2026-02-24 01:03:03 -0500

    take 7

--- src/index.js
+++ src/index.js
@@ -1,80 +1,80 @@
-import nlp from 'compromise'
-import {rando} from '@nastyox/rando.js'
+// import nlp from 'compromise'
+// import {rando} from '@nastyox/rando.js'
 import {createServer} from 'http'
 
-function apply(str, pos, func) {
-    var doc = nlp(str);
-    doc = doc.normalize('heavy');
-    doc = doc[pos+'s']()[func]();
-    return doc
-} 
+// function apply(str, pos, func) {
+//     var doc = nlp(str);
+//     doc = doc.normalize('heavy');
+//     doc = doc[pos+'s']()[func]();
+//     return doc
+// } 
 
-var data = await fetch('https://doikayt.vercel.app/api');
-var obj = await data.json();
-var objArr = Object.values(obj).flat(2);
-var sentences = [];
-for (let i = 0; i < objArr.length; i++) {
-    let desc = objArr[i]?.description;
-    if (!desc) {
-        continue;
-    }
-    if (desc.includes('...')) {
-        continue;
-    } 
-    let sentence = Array.isArray(desc.split('.')) ? desc.split('. ')[0]+'.' : desc;
-    sentences.push(sentence);
-}
+// var data = await fetch('https://doikayt.vercel.app/api');
+// var obj = await data.json();
+// var objArr = Object.values(obj).flat(2);
+// var sentences = [];
+// for (let i = 0; i < objArr.length; i++) {
+//     let desc = objArr[i]?.description;
+//     if (!desc) {
+//         continue;
+//     }
+//     if (desc.includes('...')) {
+//         continue;
+//     } 
+//     let sentence = Array.isArray(desc.split('.')) ? desc.split('. ')[0]+'.' : desc;
+//     sentences.push(sentence);
+// }
 
-var paragraph = sentences.join(' ').toLowerCase()
+// var paragraph = sentences.join(' ').toLowerCase()
 
-var doc = nlp(paragraph)
+// var doc = nlp(paragraph)
 
-doc.normalize('medium')
+// doc.normalize('medium')
 
-doc.unique()
+// doc.unique()
 
-var verbArr = doc.verbs().out('array')
-var nounArr = nounArr = doc.nouns().out('array')
+// var verbArr = doc.verbs().out('array')
+// var nounArr = nounArr = doc.nouns().out('array')
 
-function verb() {
-    var randomVerb = verbArr[rando(verbArr.length-1)]
-    return randomVerb
-}
-function noun() {
-    var randomNoun = nounArr[rando(nounArr.length-1)]
-    return randomNoun
-}
+// function verb() {
+//     var randomVerb = verbArr[rando(verbArr.length-1)]
+//     return randomVerb
+// }
+// function noun() {
+//     var randomNoun = nounArr[rando(nounArr.length-1)]
+//     return randomNoun
+// }
 
 
-var thoughts = ['and did u know I brushed my teeth today', 'and did you know the patriots lost', 'and did you know the sky is turning red', 'did you know', 'and did you know that god isn\'t dead', 'and where is my phone', 'and where did it go', '????', 'and what are your thoughts on genocide', 'and are trans people real', 'and what about antisemitism?', 'and is any of this new', 'and what is knowledge anyway', 'and is this a fungal infection', 'and what\'s your ideal way to die']
+// var thoughts = ['and did u know I brushed my teeth today', 'and did you know the patriots lost', 'and did you know the sky is turning red', 'did you know', 'and did you know that god isn\'t dead', 'and where is my phone', 'and where did it go', '????', 'and what are your thoughts on genocide', 'and are trans people real', 'and what about antisemitism?', 'and is any of this new', 'and what is knowledge anyway', 'and is this a fungal infection', 'and what\'s your ideal way to die']
 
 
 
 createServer((req,res)=>{
-    var preHtml =`emma@emmas-MacBook-Air worldburning %
+//     var preHtml =`emma@emmas-MacBook-Air worldburning %
 
-  IS IT POETRY
+//   IS IT POETRY
 
-that ${noun() + ' ' + verb() + ' ' + noun() + '  &'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
-${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// that ${noun() + ' ' + verb() + ' ' + noun() + '  &'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
+// ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
 
 
-      ${rando(thoughts).value}
+//       ${rando(thoughts).value}
       
-`
+// `
 
     var html = `<!DOCTYPE html>
 <html style="filter:invert(100%)">
@@ -82,9 +82,9 @@ ${noun() + ' ' + verb() + ' ' + noun() + '--&'}
         <title>by emma kn</title>
     </head>
     <body>
-        <pre>${preHtml}</pre>
+        <pre>${'preHtml'}</pre>
     </body>
 </html>`
     res.writeHead(200, {'Content-Type':'text/html'})
     res.end(html)
-}).listen(1080)
+}).listen(8080)
